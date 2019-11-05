be8d7f2452438d52d0ff5050fed8488da5dde57d
Ernesto Ulate Ramirez <ernesto.ulate@gmail.com>
2019-11-04 19:09:09 -0600

    Add asserting the start

--- talleres/taller-11/fsm-cafe/fsm_coffee.cpp
+++ talleres/taller-11/fsm-cafe/fsm_coffee.cpp
@@ -1,38 +1,118 @@
 #include "systemc.h"
 
+#define BLACK_COFFEE 1
+#define LATTE_COFFEE 2
+#define CAPUCCINO_COFFEE 4
+
 SC_MODULE (fsm_coffee)
 {
     sc_in<bool>             clk;
     sc_in<bool>             reset;
     sc_in<bool>             enable;
+    sc_in<bool>             start;
     sc_in< sc_uint<3> >     select;
+    sc_in<bool>             delivered;
     sc_out< sc_uint<4> >    state;
+    sc_out<bool>            dropping_coffee;
+    sc_out<bool>            dropping_milk;
+    sc_out<bool>            dropping_sugar;
+
+    enum states
+    {
+        Idle_state,
+        Coffee_selected_state,
+        Drop_coffee_state,
+        Drop_milk_state,
+        Drop_sugar_state,
+        Waiting_deliver_state,
+    };
 
     // local variables
     sc_uint<4> actual_state;
     sc_uint<4> next_state;
+    sc_uint<3> selection;
 
     void fsm_coffee_run ()
     {
         if( reset.read() == 1 )
         {
-            actual_state = 0;
-            next_state = 0;
+            actual_state = Idle_state;
+            next_state = Idle_state;
+
+            dropping_coffee.write(0);
+            dropping_milk.write(0);
+            dropping_sugar.write(0);
         }
         else if( enable.read() == 1 )
         {
             switch (actual_state)
             {
-            case 0:
-                next_state = 1;
+            case Idle_state:
+                if( start.read() )
+                {
+                    next_state = Coffee_selected_state;
+                }
                 break;
             
-            case 1:
-                next_state = 2;
+            case Coffee_selected_state:
+                if( select.read() == BLACK_COFFEE )
+                {
+                    selection = 0x1;
+                }
+                else if ( select.read() == LATTE_COFFEE )
+                {
+                    selection = 0x3;
+                }
+                else if ( select.read() == CAPUCCINO_COFFEE )
+                {
+                    selection = 0x7;
+                }
+
+                next_state = Drop_coffee_state;
                 break;
 
-            case 2:
-                next_state = 0;
+            case Drop_coffee_state:
+                if( selection >= 1 )
+                {
+                    dropping_coffee.write(1);
+                    dropping_milk.write(0);
+                    dropping_sugar.write(0);
+                }
+
+                next_state = Drop_milk_state;
+                break;
+
+            case Drop_milk_state:
+                if( selection >= 3 )
+                {
+                    dropping_coffee.write(0);
+                    dropping_milk.write(1);
+                    dropping_sugar.write(0);
+
+                }
+
+                next_state = Drop_sugar_state;
+                break;
+
+            case Drop_sugar_state:
+                if( selection >= 7 )
+                {
+                    dropping_coffee.write(0);
+                    dropping_milk.write(0);
+                    dropping_sugar.write(1);
+
+                }
+
+                next_state = Drop_sugar_state;
+                break;
+
+            case Waiting_deliver_state:
+                if( delivered.read() == 1 )
+                {
+                    next_state = Idle_state;
+                }
+
+                next_state = Waiting_deliver_state;
                 break;
 
             default:
--- talleres/taller-11/fsm-cafe/test_fsm_coffee.cpp
+++ talleres/taller-11/fsm-cafe/test_fsm_coffee.cpp
@@ -3,19 +3,30 @@
 
 int sc_main (int agrc, char *argv[])
 {
-    sc_signal<bool>             clock;
-    sc_signal<bool>             reset;
-    sc_signal<bool>             enable;
-    sc_signal< sc_uint<3> >     select;
+    sc_signal<bool>             clock_in;
+    sc_signal<bool>             reset_in;
+    sc_signal<bool>             enable_in;
+    sc_signal<bool>             start_in;
+    sc_signal< sc_uint<3> >     select_in;
+    sc_signal<bool>             delivered_in;
     sc_signal< sc_uint<4> >     state_out;
+    sc_signal<bool>             dropping_coffee_out;
+    sc_signal<bool>             dropping_milk_out;
+    sc_signal<bool>             dropping_sugar_out;
     int i = 0;
 
     // Connect the DUT
-    fsm_coffee coffe_machine("COFFEE-MACHINE");
-    coffe_machine.clk(clock);
-    coffe_machine.reset(reset);
-    coffe_machine.enable(enable);
-    coffe_machine.state(state_out);
+    fsm_coffee coffee_machine("COFFEE-MACHINE");
+    coffee_machine.clk(clock_in);
+    coffee_machine.reset(reset_in);
+    coffee_machine.enable(enable_in);
+    coffee_machine.start(start_in);
+    coffee_machine.select(select_in);
+    coffee_machine.delivered(delivered_in);
+    coffee_machine.state(state_out);
+    coffee_machine.dropping_coffee(dropping_coffee_out);
+    coffee_machine.dropping_milk(dropping_milk_out);
+    coffee_machine.dropping_sugar(dropping_sugar_out);
 
     sc_start(1, SC_NS);
 
@@ -23,63 +34,98 @@ int sc_main (int agrc, char *argv[])
     sc_trace_file *wf = sc_create_vcd_trace_file("coffee-machine");
 
     // Dump the desire signals
-    sc_trace(   wf, clock,      "clk"       );
-    sc_trace(   wf, reset,      "reset"     );
-    sc_trace(   wf, enable,     "enable"    );
-    sc_trace(   wf, state_out,  "state_out" );
+    sc_trace(wf, clock_in, "clk_in");
+    sc_trace(wf, reset_in, "reset_in");
+    sc_trace(wf, enable_in, "enable_in");
+    sc_trace(wf, start_in, "start_in");
+    sc_trace(wf, select_in, "select_in");
+    sc_trace(wf, delivered_in, "delivered_in");
+    sc_trace(wf, state_out, "state_out");
+    sc_trace(wf, dropping_coffee_out, "dropping_coffee_out");
+    sc_trace(wf, dropping_milk_out, "dropping_milk_out");
+    sc_trace(wf, dropping_sugar_out, "dropping_sugar_out");
+
 
     // Initialize simulation
-    reset = 0;
-    enable = 0;
+    clock_in = 0;
+    reset_in = 0;
+    enable_in = 0;
+    start_in = 0;
+    select_in = 0;
+    delivered_in = 0;
     for (i = 0; i < 5; i++)
     {
-        clock = 0;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
-        clock = 1;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
     }
 
-    // Assert the reset
-    reset = 1;
+    // Assert the reset_in
+    reset_in = 1;
     cout << "@" << sc_time_stamp() << " :: Asserting_reset" << endl;
     for (i = 0; i < 10; i++)
     {
-        clock = 0;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
-        clock = 1;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
     }
 
-    // De-assert the reset
-    reset = 0;
+    // De-assert the reset_in
+    reset_in = 0;
     cout << "@" << sc_time_stamp() << " :: De-asserting_reset" << endl;
     for (i = 0; i < 10; i++)
     {
-        clock = 0;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
-        clock = 1;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
     }
 
-    // Assert the enable
-    enable = 1;
+    // Assert the enable_in
+    enable_in = 1;
     cout << "@" << sc_time_stamp() << " :: Asserting_enable" << endl;
     for (i = 0; i < 20; i++)
     {
-        clock = 0;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
-        clock = 1;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
     }
 
-    // De-assert the enable
-    enable = 0;
+    // Assert the start_in
+    start_in = 1;
+    cout << "@" << sc_time_stamp() << " :: Asserting_start" << endl;
+    for (i = 0; i < 20; i++)
+    {
+        clock_in = ~clock_in;
+        sc_start(1, SC_NS);
+        clock_in = ~clock_in;
+        sc_start(1, SC_NS);
+    }
+
+    
+
+    // De-assert the start_in
+    start_in = 0;
+    cout << "@" << sc_time_stamp() << " :: De-asserting_start" << endl;
+    for (i = 0; i < 5; i++)
+    {
+        clock_in = ~clock_in;
+        sc_start(1, SC_NS);
+        clock_in = ~clock_in;
+        sc_start(1, SC_NS);
+    }
+
+    // De-assert the enable_in
+    enable_in = 0;
     cout << "@" << sc_time_stamp() << " :: De-asserting_enable" << endl;
     for (i = 0; i < 5; i++)
     {
-        clock = 0;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
-        clock = 1;
+        clock_in = ~clock_in;
         sc_start(1, SC_NS);
     }
 
